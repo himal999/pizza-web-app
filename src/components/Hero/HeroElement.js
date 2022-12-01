ddef87b2985bb1c0fc3645f2969294df1959279e
himal999 <himal.official99@gmail.com>
2022-12-01 08:41:05 +0530

    complete hero section

--- src/components/Hero/HeroElement.js
+++ src/components/Hero/HeroElement.js
@@ -7,3 +7,49 @@ export const HeroContainer = styled.div`
   background-position: center;
   background-size: cover;
 `;
+export const HeroContent = styled.div`
+  height: calc(100vh-80px);
+  max-height: 100%;
+  width: 100vw;
+  padding: 0rem calc((100vw - 1300px) / 2);
+`;
+
+export const HeroItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  height: 100vh;
+  max-height: 100%;
+  padding: 0rem 2rem;
+  width: 650px;
+  color: #fff;
+  text-transform: uppercase;
+  line-height: 1;
+  font-weight: bold;
+`;
+export const HeroTitle = styled.div`
+  font-size: clamp(2.5rem, 10vw, 5rem);
+  margin-bottom: 1rem;
+  box-shadow: 0px 5px #e9ba23;
+  letter-spacing: 3px;
+`;
+export const HeroSubTitle = styled.div`
+  font-size: clamp(1rem, 10vw, 2rem);
+  margin-bottom: 2rem;
+`;
+export const HeroButton = styled.div`
+  font-size: 1.4rem;
+  padding: 1rem 1.5rem;
+  border: none;
+  background: #e31837;
+  color: #fff;
+  transition: 0.2s ease-out;
+  cursor: pointer;
+
+  &:hover {
+    background: #fff;
+    color: #e31837;
+    transition: 0.2s ease-out;
+  }
+`;
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { HeroContainer } from './HeroElement';
+import {
+  HeroContainer,
+  HeroContent,
+  HeroItems,
+  HeroTitle,
+  HeroSubTitle,
+  HeroButton,
+} from './HeroElement';
 import Navbar from '../Navbar';
 const Hero = () => {
   return (
     <HeroContainer>
       <Navbar />
+      <HeroContent>
+        <HeroItems>
+          <HeroTitle>FRESH PIZZA</HeroTitle>
+          <HeroSubTitle>Are u ready in order? Hurry up</HeroSubTitle>
+          <HeroButton>Order Now</HeroButton>
+        </HeroItems>
+      </HeroContent>
     </HeroContainer>
   );
 };
